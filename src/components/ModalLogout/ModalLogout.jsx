754d13822003536c573bb4291c3f12af4f09b56d
Katarzyna Leoniuk <kasia.leoniuk@gmail.com>
2023-09-24 10:21:17 +0200

    Add: code and styles for ModalLogout
    Added: logout in auth-operations.js and global redux for opening and closing modal

---
+++ src/components/ModalLogout/ModalLogout.jsx
@@ -0,0 +1,69 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+
+import { toggleModalOpen, setModalType } from "../../redux/global/global-slice";
+import { logout } from "redux/auth/auth-operations";
+
+import css from "./ModalLogout.module.css";
+
+export const ModalLogout = () => {
+  const isModalOpen = useSelector((state) => state.global.isModalOpen);
+  const modalType = useSelector((state) => state.global.modalType);
+  const dispatch = useDispatch();
+
+  const handleBackdropClick = (e) => {
+    if (e.target === e.currentTarget) {
+      dispatch(toggleModalOpen());
+      dispatch(setModalType(null));
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.code === "Escape") {
+        dispatch(toggleModalOpen());
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [dispatch]);
+
+  return (
+    <>
+      {isModalOpen && modalType === "logout" && (
+        <div className={css.overlay} onClick={handleBackdropClick}>
+          <div className={css.modalContainer}>
+            <p className={css.text}>Are you sure you want to log out?</p>
+            <div className={css.buttonContainer}>
+              <button
+                title="logout"
+                className={css.button}
+                onClick={() => {
+                  dispatch(logout());
+                  dispatch(toggleModalOpen());
+                  dispatch(setModalType(null));
+                }}
+              >
+                LOG ME OUT
+              </button>
+              <button
+                title="cancel"
+                className={css.button}
+                onClick={() => {
+                  dispatch(toggleModalOpen());
+                  dispatch(setModalType(null));
+                }}
+              >
+                CANCEL
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
