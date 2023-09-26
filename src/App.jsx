090e3d8b2b9d4cb32db4c20f85ef9df5dfaf18eb
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-09-26 14:48:49 +0200

    PM (update)
    1. Nowy branch paulina-DiagramTab-3
    2. Przebudowa tabeli DiagramTab
    3. Ustawienie tzw. enum-a statisticsList i mockowanie
    4. Stylowanie tabeli DiagramTab

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-
-import Statistics from "./pages/Statistics";
-// import StatisticsPage from "./pages/StatisticsPage/StatisticsPage";
-
+import StatisticsPage from "./pages/StatisticsPage/StatisticsPage";
 
 export const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
-      <Statistics />
-      {/* <StatisticsPage /> */}
-
+      <StatisticsPage />
     </div>
   );
 };
