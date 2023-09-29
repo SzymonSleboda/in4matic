10f0133cea8548e53b6bcae6e7547a04c8e77a1d
SzymonSleboda <szymonskeboda@gmail.com>
2023-09-29 10:18:39 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-//import Statistics from "./pages/Statistics";
-import StatisticsPage from "./pages/StatisticsPage/StatisticsPage";
-// import DashboardPage from "./pages/DashboardPage/DashboardPage";
-
+import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import { lazy, Suspense, useEffect, useState } from 'react';
+// import Layout from './layout/Layout';
+// import WithAuthRedirect from './routes/WithAuthRedirect';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useSelector } from 'react-redux';
+// import { ModalLogout } from './components/ModalLogout/ModalLogout';
+// import { AddTransactionModal } from './components/AddTransactionModal/AddTransactionModal';
+// import { EditTransactionModal } from './components/EditTransactionModal/EditTransactionModal';
+// import { LoaderGlobal } from './components/Loader/LoaderGlobal';
+import { LoginPage } from "./pages/LoginPage/LoginPage";
+import { RegistrationPage } from "./pages/RegistrationPage/RegistrationPage";
+const DashboardPage = lazy(() => import('./pages/DashboardPage/DashboardPage'));
+const Statistics = lazy(() => import('./pages/StatisticsPage/StatisticsPage'));
 export const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <div >
-        {/* <DashboardPage /> */}
-        {/* <Statistics /> */}
-        <StatisticsPage />
-      </div>
-    </div>
+    <>
+      {/* {showLoader && !delayCompleted ? <LoaderGlobal /> : null}
+      {isModalLogoutOpen && <ModalLogout />}
+      {isModalAddTransactionOpen && <AddTransactionModal />}
+      {isModalEditTransactionOpen && <EditTransactionModal />} */}
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegistrationPage />} />
+          <Route path="/home" element={<DashboardPage />} />
+          <Route path="/diagram" element={<Statistics />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+        <ToastContainer position="top-center" autoClose={2500} pauseOnHover={false} />
+      </Suspense>
+    </>
   );
 };
