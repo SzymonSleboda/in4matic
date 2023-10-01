d94147db8394fa9f94a1112867bf1a4bd03d84a0
SzymonSleboda <szymonskeboda@gmail.com>
2023-10-01 11:37:52 +0200

    Update DashboardPage.jsx

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,3 +1,4 @@
+import React from "react"; // Import React
 import s from "../DashboardPage/DashboardPage.module.css";
 import Header from "../../components/Header/Header";
 import HomeTab from "../../components/HomeTab/HomeTab";
@@ -9,9 +10,10 @@ import { ButtonAddTransactions } from "../../components/ButtonAddTransactions/Bu
 import { useDeviceSize } from "../../hooks/useDeviceSize";
 import { useLocation } from "react-router-dom";
 
-export default function DashboardPage() {
+export default function DashboardPage({ children }) {
   const { deviceType } = useDeviceSize();
   const location = useLocation();
+
   if (deviceType === "mobile") {
     return (
       <div className={s.dashboard}>
@@ -33,43 +35,44 @@ export default function DashboardPage() {
         </div>
       </div>
     );
-  }
-  return (
-    <div className={s.dashboard}>
-      <Header />
-      <div className={s.dashboard__content}>
-        <div className={s.dashboard__left}>
-          <div className={s.dashboard__navigation}>
-            <Navigation />
-            <Balance />
-          </div>
-          {deviceType === "desktop" && (
-            <div className={s.dashboard__currency}>
-              <Currency />
+  } else {
+    return (
+      <div className={s.dashboard}>
+        <Header />
+        <div className={s.dashboard__content}>
+          <div className={s.dashboard__left}>
+            <div className={s.dashboard__navigation}>
+              <Navigation />
+              <Balance />
             </div>
-          )}
-        </div>
-        <div className={s.dashboard__right}>
-          {deviceType === "desktop" ? (
+            {deviceType === "desktop" && (
+              <div className={s.dashboard__currency}>
+                <Currency />
+              </div>
+            )}
+          </div>
+          <div className={s.dashboard__right}>
+            {deviceType === "desktop" ? (
+              <div className={s.dashboard__table}>
+                <Table />
+              </div>
+            ) : (
+              <div className={s.dashboard__currency}>
+                <Currency />
+              </div>
+            )}
+          </div>
+          {deviceType !== "desktop" && (
             <div className={s.dashboard__table}>
               <Table />
             </div>
-          ) : (
-            <div className={s.dashboard__currency}>
-              <Currency />
-            </div>
           )}
-        </div>
-        {deviceType !== "desktop" && (
-          <div className={s.dashboard__table}>
-            <Table />
+          <div>
+            <ButtonAddTransactions />
           </div>
-        )}
-        <div>
-          <ButtonAddTransactions />
+          {deviceType !== "desktop" && children}
         </div>
-        {deviceType !== "desktop" && children}
       </div>
-    </div>
-  );
+    );
+  }
 }
