a2ee59a78d344b92e624618905670c3322e2fdd7
SzymonSleboda <szymonskeboda@gmail.com>
2023-10-01 11:53:09 +0200

    Update Currency.jsx

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -1,13 +1,13 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import s from "../Currency/Currency.module.css"
 
 export default function Currency() {
-    const [USD, setUSD] = useState({ buy: '00.00', sale: '00.00' });
-    const [EUR, setEUR] = useState({ buy: '00.00', sale: '00.00' });
+    // const [USD, setUSD] = useState({ buy: '00.00', sale: '00.00' });
+    // const [EUR, setEUR] = useState({ buy: '00.00', sale: '00.00' });
 
-    useEffect(() => { 
+    // useEffect(() => { 
 
-    });
+    // });
 
     return (
       <div className={s.currency}>
@@ -19,13 +19,13 @@ export default function Currency() {
         <ul className={s.currency__list}>
           <li className={s.currency__item}>
             <span>USD</span>
-            <span>{USD.buy}</span>
-            <span>{USD.sale}</span>
+            <span>00.00</span>
+            <span>00.00</span>
           </li>
           <li className={s.currency__item}>
             <span>EUR</span>
-            <span>{EUR.buy}</span>
-            <span>{EUR.sale}</span>
+            <span>00.00</span>
+            <span>00.00</span>
           </li>
         </ul>
       </div>
