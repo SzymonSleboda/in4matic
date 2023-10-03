0cc23e89d23c9f65341d5c6c66d7bf90efde212c
SzymonSleboda <szymonskeboda@gmail.com>
2023-10-03 17:12:23 +0200

    Update AddTransaction.jsx

--- src/components/AddTransaction/AddTransaction.jsx
+++ src/components/AddTransaction/AddTransaction.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-
 import { Formik, Field } from "formik";
 import { useMediaQuery } from "react-responsive";
 import { date, object, string } from "yup";
-
 import { toggleTransactionModalOpen } from "../../redux/global/global-slice";
 import { selectIsTransactionModalOpen } from "../../redux/global/global-selectors";
 import { getCategories } from "../../redux/categories/categories-operations";
@@ -14,69 +12,55 @@ import { getTransactions } from "../../redux/transactions/transactions-operation
 import { selectToken } from "../../redux/auth/auth-selectors";
 import { selectId } from "../../redux/auth/auth-selectors";
 import { getCurrentUser } from "../../redux/user/user-operations";
-
 import DateComponent from "./DateComponent/DateComponent";
 import SelectComponent from "./SelectComponent/SelectComponent.jsx";
 import css from "./AddTransaction.module.css";
-
 // to utils
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
 // should be extracted to constants
 const TRANSACTION_TYPE = {
   EXPENSE: "EXPENSE",
   INCOME: "INCOME",
 };
-
 const defaultState = {
   type: TRANSACTION_TYPE.EXPENSE,
-  categoryId: null,
+  category: "",
   amount: "",
   date: new Date(),
   comment: " ",
 };
-
 export const AddTransaction = () => {
   const isTransactionModalOpen = useSelector(selectIsTransactionModalOpen); // could be a custom hook exposing modal state.
   const categories = useSelector(selectCategories);
   const token = useSelector(selectToken);
   const [transactionState, setTransactionState] = useState(defaultState);
   const id = useSelector(selectId);
-
   const dispatch = useDispatch();
-
   useEffect(() => {
     if (!isTransactionModalOpen) return;
-
     if (!Array.isArray(categories) || categories.length === 0) {
       dispatch(getCategories());
     }
   }, [isTransactionModalOpen, dispatch, categories]);
-
   const handleBackdropClick = (e) => {
     if (e.target === e.currentTarget) {
       dispatch(toggleTransactionModalOpen());
     }
   };
-
   useEffect(() => {
     const handleKeyDown = (e) => {
       if (e.code === "Escape") {
         dispatch(toggleTransactionModalOpen());
       }
     };
-
     window.addEventListener("keydown", handleKeyDown);
-
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
   }, [dispatch]);
-
   const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
-
   const handleCheckboxChange = (e) => {
     if (transactionState.type === TRANSACTION_TYPE.EXPENSE) {
       setTransactionState((prev) => ({
@@ -92,40 +76,23 @@ export const AddTransaction = () => {
       e.target.setAttribute("checked", "true");
     }
   };
-
   const handleSetCategory = (category) => {
-    setTransactionState((prev) => ({ ...prev, categoryId: category }));
+    setTransactionState((prev) => ({ ...prev, category: category }));
   };
-
   const handleDateChange = (selectedDate) => {
     setTransactionState((prev) => ({ ...prev, date: selectedDate._d }));
   };
-
-  console.log({ token })
   const handleSubmit = async (values, actions) => {
     const { amount, comment } = values;
     const month = transactionState.date.getMonth() + 1;
     const year = transactionState.date.getFullYear();
-
     let selectedCategoryName = "Income";
-
-    console.log("dupa 2")
     if (transactionState.type === TRANSACTION_TYPE.EXPENSE) {
       const selectedCategory = categories.find(
-        (el) => el.id === transactionState.categoryId
-      ) ?? { category: "Products" };
-
-      console.log({ selectedCategory });
-
-      selectedCategoryName = capitalizeFirstLetter(selectedCategory.category);
-      console.log({ selectedCategoryName });
+        (el) => el.name === transactionState.category
+      );
+      selectedCategoryName = capitalizeFirstLetter(selectedCategory.name);
     }
-
-    console.log("SDFADFAS")
-
-    // await sleep(5000);
-
-
     const formData = {
       category: selectedCategoryName,
       type: transactionState.type === TRANSACTION_TYPE.INCOME ? true : false,
@@ -137,11 +104,8 @@ export const AddTransaction = () => {
       owner: id,
       isIncome: selectedCategoryName === "Income",
     };
-
     console.log({ formData });
-
     dispatch(addTransaction(formData));
-
     setTransactionState((prev) => ({
       ...prev,
       type: TRANSACTION_TYPE.EXPENSE,
@@ -151,7 +115,6 @@ export const AddTransaction = () => {
     dispatch(getTransactions({ token }));
     dispatch(getCurrentUser({ token }));
   };
-
   let validationSchema = object({
     amount: string()
       .required("Amount is required")
@@ -161,7 +124,6 @@ export const AddTransaction = () => {
       .default(() => new Date()),
     type: string().required("Required"),
   });
-
   return (
     <Formik
       initialValues={transactionState}
@@ -171,120 +133,122 @@ export const AddTransaction = () => {
         setTransactionState((prev) => ({ ...prev, nextValues }));
       }}
     >
-      {(formik) => 
-      <form className={css.modalForm} onSubmit={formik.handleSubmit}>
-        {" "}
-        {isTransactionModalOpen && (
-          <div className={css.overlay} onClick={handleBackdropClick}>
-            <div className={css.modalContainer}>
-              <h2 className={css.mainTitle}>Add transaction</h2>
-              <div className={css.modalContainerTransaction}>
-                <div className={css.switcher} style={{ position: "relative" }}>
-                  <span
-                    className={
-                      transactionState.type === TRANSACTION_TYPE.INCOME
-                        ? css.income
-                        : ""
-                    }
+      {(formik) => (
+        <form className={css.modalForm} onSubmit={formik.handleSubmit}>
+          {" "}
+          {isTransactionModalOpen && (
+            <div className={css.overlay} onClick={handleBackdropClick}>
+              <div className={css.modalContainer}>
+                <h2 className={css.mainTitle}>Add transaction</h2>
+                <div className={css.modalContainerTransaction}>
+                  <div
+                    className={css.switcher}
+                    style={{ position: "relative" }}
                   >
-                    Income
-                  </span>
-                  <label className={css.switcherWrapper}>
-                    <Field
-                      type="checkbox"
-                      name="type"
-                      onChange={handleCheckboxChange}
-                      className={css.switcherCheckbox}
-                      checked={
-                        transactionState.type === TRANSACTION_TYPE.EXPENSE
-                          ? true
-                          : false
+                    <span
+                      className={
+                        transactionState.type === TRANSACTION_TYPE.INCOME
+                          ? css.income
+                          : ""
                       }
-                    />
-                    <span className={css.switcherToggle}></span>
-                  </label>
-                  <span
-                    className={
-                      transactionState.type === TRANSACTION_TYPE.EXPENSE
-                        ? css.expense
-                        : ""
-                    }
-                  >
-                    Expense
-                  </span>
-                </div>
-                {transactionState.type === TRANSACTION_TYPE.EXPENSE && (
-                  <div className={css.categoryWrapper}>
-                    <label>
+                    >
+                      Income
+                    </span>
+                    <label className={css.switcherWrapper}>
                       <Field
-                        className={css.select}
-                        name="category"
-                        placeholder="Select a category"
-                        component={SelectComponent}
-                        options={categories.map((option) => ({
-                          value: option.id,
-                          label: option.category,
-                        }))}
-                        handleSetCategory={handleSetCategory}
+                        type="checkbox"
+                        name="type"
+                        onChange={handleCheckboxChange}
+                        className={css.switcherCheckbox}
+                        checked={
+                          transactionState.type === TRANSACTION_TYPE.EXPENSE
+                            ? true
+                            : false
+                        }
                       />
+                      <span className={css.switcherToggle}></span>
                     </label>
+                    <span
+                      className={
+                        transactionState.type === TRANSACTION_TYPE.EXPENSE
+                          ? css.expense
+                          : ""
+                      }
+                    >
+                      Expense
+                    </span>
                   </div>
-                )}
-
-                <div className={css.amountDateWrapper}>
-                  <div className={css.amountWrapper}>
-                    <label>
-                      <Field
-                        type="number"
-                        placeholder="0.00"
-                        name="amount"
-                        className={css.amount}
-                      />
-                    </label>
+                  {transactionState.type === TRANSACTION_TYPE.EXPENSE && (
+                    <div className={css.categoryWrapper}>
+                      <label>
+                        <Field
+                          className={css.select}
+                          name="category"
+                          placeholder="Select a category"
+                          component={SelectComponent}
+                          options={categories.map((option) => ({
+                            value: option.id,
+                            label: option.category,
+                          }))}
+                          handleSetCategory={handleSetCategory}
+                        />
+                      </label>
+                    </div>
+                  )}
+                  <div className={css.amountDateWrapper}>
+                    <div className={css.amountWrapper}>
+                      <label>
+                        <Field
+                          type="number"
+                          placeholder="0.00"
+                          name="amount"
+                          className={css.amount}
+                        />
+                      </label>
+                    </div>
+                    <div className={css.dateWrapper}>
+                      <label>
+                        <Field
+                          as="date"
+                          component={DateComponent}
+                          className={css.date}
+                          name="date"
+                          dateFormat="DD.MM.YYYY"
+                          timeFormat={false}
+                          value={transactionState.date}
+                          onChange={handleDateChange}
+                        />
+                      </label>
+                    </div>
                   </div>
-                  <div className={css.dateWrapper}>
-                    <label>
-                      <Field
-                        as="date"
-                        component={DateComponent}
-                        className={css.date}
-                        name="date"
-                        dateFormat="DD.MM.YYYY"
-                        timeFormat={false}
-                        value={transactionState.date}
-                        onChange={handleDateChange}
-                      />
-                    </label>
+                  <Field
+                    className={css.textarea}
+                    rows={isMobile ? "5" : "1"}
+                    type="text"
+                    placeholder="Comment"
+                    name="comment"
+                  />
+                  <div className={css.buttonContainer}>
+                    <button title="add" className={css.button} type="submit">
+                      ADD
+                    </button>
+                    <button
+                      type="button"
+                      title="cancel"
+                      className={css.button}
+                      onClick={() => {
+                        dispatch(toggleTransactionModalOpen());
+                      }}
+                    >
+                      CANCEL
+                    </button>
                   </div>
-                </div>
-                <Field
-                  className={css.textarea}
-                  rows={isMobile ? "5" : "1"}
-                  type="text"
-                  placeholder="Comment"
-                  name="comment"
-                />
-                <div className={css.buttonContainer}>
-                  <button title="add" className={css.button} type="submit">
-                    ADD
-                  </button>
-                  <button
-                    type="button"
-                    title="cancel"
-                    className={css.button}
-                    onClick={() => {
-                      dispatch(toggleTransactionModalOpen());
-                    }}
-                  >
-                    CANCEL
-                  </button>
                 </div>
               </div>
             </div>
-          </div>
-        )}
-      </form>
-    }
+          )}
+        </form>
+      )}
     </Formik>
   );
 };
